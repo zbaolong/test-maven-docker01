62814b63c4075eb4a40cc666e9e1217c227dc07a
zbaolong <292022420@qq.com>
2021-01-18 15:50:35 +0800

    add success!!

--- src/main/java/com/h3c/testmavendocker01/controller/HelloController.java
+++ src/main/java/com/h3c/testmavendocker01/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
     @ResponseBody
@@ -13,6 +17,14 @@ public class HelloController {
     }
     @RequestMapping("/success")
     public String success(){
+        return "success";
+    }
+    //查询一些数据放到页面
+    @RequestMapping("/show")
+    public String show_data(Map<String,Object> map){
+        map.put("hello","<h1>您好</h1>");
+        map.put("users", Arrays.asList("zsan","lisi"));
+
         return "success";
     }
 
