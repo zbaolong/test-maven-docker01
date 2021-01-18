c2c9f5c3607cf9fe58d5070f050f84bd78f66bc6
zbaolong <292022420@qq.com>
2021-01-18 09:52:31 +0800

    first commit

---
+++ src/main/java/com/h3c/testmavendocker01/TestMavenDocker01Application.java
@@ -0,0 +1,13 @@
+package com.h3c.testmavendocker01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMavenDocker01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMavenDocker01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/h3c/testmavendocker01/TestMavenDocker01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h3c.testmavendocker01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMavenDocker01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
